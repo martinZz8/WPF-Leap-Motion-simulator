ab4bbad78b51c68674fa9b372e536821b3ad1695
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2021-09-18 18:37:43 +0200

    First code written

---
+++ WPF-Leap-Motion-simulator/WPF-Leap-Motion-simulator/LeapTracker/LeapEventListener/LeapEventListener.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Leap;
+using System.Threading;
+
+//LeapEventDelegate
+using WPF_Leap_Motion_simulator.LeapTracker.LeapEventDelegate;
+
+namespace WPF_Leap_Motion_simulator.LeapTracker.LeapEventListener
+{
+    public class LeapEventListener : Listener
+    {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventListener(ILeapEventDelegate delegateObject)
+        {
+            eventDelegate = delegateObject;
+        }
+
+        public override void OnInit(Controller controller)
+        {
+            Console.WriteLine("Initialized");
+            eventDelegate.LeapEventNotification("onInit");
+        }
+        public override void OnConnect(Controller controller)
+        {
+            controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            controller.Config.SetFloat("Gesture.Circle.MinRadius", 40.0f);
+            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+            controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+
+            Console.WriteLine("Connected");
+            eventDelegate.LeapEventNotification("onConnect");
+        }
+        public override void OnFrame(Controller controller)
+        {
+            //Processing code of latest acquired frame
+            Frame frame = controller.Frame();
+            Console.WriteLine("----------Frame id: " + frame.Id.ToString() + "----------------");
+            Console.WriteLine("Frame timestamp: " + frame.Timestamp.ToString());
+            Console.WriteLine("Current FPS: " + frame.CurrentFramesPerSecond.ToString());
+            Console.WriteLine("Is valid: " + frame.IsValid.ToString());
+            Console.WriteLine("Frame gestures count: " + frame.Gestures().Count.ToString());
+            Console.WriteLine("Images count: " + frame.Images.Count.ToString());
+            Console.Write("\n\n");
+            GestureDetection(frame.Gestures());
+
+            //TO DELETE
+            //if(frame.Fingers.Count > 0)
+            //{
+            //    Console.WriteLine("Finger one: " +frame.Fingers[0].Bone(Bone.BoneType.TYPE_DISTAL).Length.ToString());
+            //}
+            
+
+            eventDelegate.LeapEventNotification("onFrame");
+            //Thread.Sleep(1000);
+        }
+        public override void OnExit(Controller controller)
+        {
+            Console.WriteLine("Exited");
+            eventDelegate.LeapEventNotification("onExit");
+        }
+        public override void OnDisconnect(Controller controller)
+        {
+            Console.WriteLine("Disconnected");
+            eventDelegate.LeapEventNotification("onDisconnect");
+        }
+
+        private void GestureDetection(GestureList gesture_list)
+        {
+            bool gd = false;
+            Console.WriteLine("HH Gesture length: \n" + gesture_list.Count);
+            foreach (Gesture gesture in gesture_list)
+            {
+                switch (gesture.Type)
+                {
+                    case Gesture.GestureType.TYPE_CIRCLE:
+                        gd = true;
+                        eventDelegate.LeapEventNotification("onCircleGestureDetected");
+                        break;
+                    case Gesture.GestureType.TYPE_SWIPE:
+                        gd = true;
+                        eventDelegate.LeapEventNotification("onSwipeGestureDetected");
+                        break;
+                    case Gesture.GestureType.TYPE_SCREEN_TAP:
+                        gd = true;
+                        eventDelegate.LeapEventNotification("onScreenTapGestureDetected");
+                        break;
+                    case Gesture.GestureType.TYPE_KEY_TAP:
+                        gd = true;
+                        eventDelegate.LeapEventNotification("onKeyTapGestureDetected");
+                        break;
+                    default:
+                        //Handle unrecognized gestures
+                        break;
+                }
+            }
+
+            if (!gd)
+            {
+                eventDelegate.LeapEventNotification("onNoGestureDetected");
+            }
+                
+        }
+    }
+}
---
+++ WPF-Leap-Motion-simulator/WPF-Leap-Motion-simulator/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// System Diagnostics for Trace.WriteLine() function
+using System.Diagnostics;
+
+//Leap
+using Leap;
+
+//LeapTracker
+using WPF_Leap_Motion_simulator.LeapTracker.LeapEventDelegate;
+using WPF_Leap_Motion_simulator.LeapTracker.LeapEventListener;
+
+namespace WPF_Leap_Motion_simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ILeapEventDelegate
+    {
+        private Controller controller;
+        private LeapEventListener listener;
+        private Boolean isClosing = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new Controller();
+            listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+        }
+
+        delegate void LeapEventDelegate(string EventName);
+        public void LeapEventNotification(string EventName)
+        {
+            if (CheckAccess())
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        Debug.WriteLine("Init");
+                        break;
+                    case "onConnect":
+                        connectHandler();
+                        break;
+                    case "onFrame":
+                        if (!isClosing)
+                            newFrameHandler(controller.Frame());
+                        break;
+                    case "onExit":
+                        Debug.WriteLine("Exit");
+                        break;
+                    case "onDisconnect":
+                        Debug.WriteLine("Disconnected");
+                        break;
+                    case "onCircleGestureDetected":
+                        Debug.WriteLine("circleGestureDetected");
+                        break;
+                    case "onSwipeGestureDetected":
+                        Debug.WriteLine("swipeGestureDetected");
+                        break;
+                    case "onScreenTapGestureDetected":
+                        Debug.WriteLine("screenTapGestureDetected");
+                        break;
+                    case "onKeyTapGestureDetected":
+                        Debug.WriteLine("keyTapGestureDetected");
+                        break;
+                    case "onNoGestureDetected":
+                        Debug.WriteLine("noGestureDetected");
+                        break;
+                }
+            }
+            else
+            {
+                Dispatcher.Invoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+
+        void connectHandler()
+        {
+            controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            controller.Config.SetFloat("Gesture.Circle.MinRadius", 40.0f);
+            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+            controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+        }
+
+        void newFrameHandler(Leap.Frame frame)
+        {
+            //this.displayID.Content = frame.Id.ToString();
+            //this.displayTimestamp.Content = frame.Timestamp.ToString();
+            displayFPS.Text = frame.CurrentFramesPerSecond.ToString();
+            //this.displayIsValid.Content = frame.IsValid.ToString();
+            //this.displayGestureCount.Content = frame.Gestures().Count.ToString();
+            //this.displayImageCount.Content = frame.Images.Count.ToString();
+        }
+
+        void MainWindow_Closing(object sender, EventArgs e)
+        {
+            isClosing = true;
+            controller.RemoveListener(listener);
+            controller.Dispose();
+        }
+    }
+}
